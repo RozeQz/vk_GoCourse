ffc654bc1988150bcb839b65bbbf04c7883417be
RozeQz <liza.roz@yandex.ru>
2022-03-08 16:32:54 +0300

    Add structures, game initialization and some methods

--- 01_intro/99_hw/game/main.go
+++ 01_intro/99_hw/game/main.go
@@ -1,9 +1,67 @@
 package main
 
-/*
-	код писать в этом файле
-	наверняка у вас будут какие-то структуры с методами, глобальные перменные ( тут можно ), функции
-*/
+import (
+	"fmt"
+	"strings"
+)
+
+type Room struct {
+	Game  *Game
+	Name  string
+	Items map[string]Item
+}
+
+type Item struct {
+	Name  string
+	Exist bool
+}
+
+type Object struct {
+	Name string
+	Lock bool
+}
+
+type Backpack struct {
+	Lock  bool
+	Items map[string]bool
+}
+
+type Player struct {
+	InRoom *Room
+	Bag    *Backpack
+}
+
+type Game struct {
+	Rooms   map[string]*Room
+	Players []Player
+}
+
+func (p *Player) Apply(o *Item) bool {
+	if o.Exist {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (p *Player) View() string {
+	var obj string
+	for _, v := range p.InRoom.Items {
+		obj += v.Name + " "
+	}
+	return "ты находишься: " + p.InRoom.Name + ". " + "предметы: " + obj
+}
+
+func (p *Player) Go(nextRoom string) string {
+	p.InRoom = g.GetRoom(nextRoom)
+	return "Вы находитесь в " + nextRoom
+}
+
+func (g *Game) GetRoom(name string) *Room {
+	return g.Rooms[name]
+}
+
+var g Game
 
 func main() {
 	/*
@@ -11,6 +69,21 @@ func main() {
 		но тогда у вас не будет работать через go run main.go
 		очень круто будет сделать построчный ввод команд тут, хотя это и не требуется по заданию
 	*/
+	initGame()
+	fmt.Println(g)
+	fmt.Println(g.Players[0].View())
+	fmt.Println(g.Players[0].Go("коридор"))
+	fmt.Println(g.Players[0].View())
+	fmt.Println(g.Players[0].Bag)
+	g.Players[0].Bag = &Backpack{
+		Lock:  true,
+		Items: map[string]bool{},
+	}
+	if g.Players[0].Bag.Lock {
+		fmt.Println("На вас надет рюкзак")
+	} else {
+		fmt.Println("На вас не надет рюкзак")
+	}
 }
 
 func initGame() {
@@ -18,6 +91,57 @@ func initGame() {
 		эта функция инициализирует игровой мир - все команты
 		если что-то было - оно корректно перезатирается
 	*/
+	g = Game{
+		Rooms:   make(map[string]*Room),
+		Players: make([]Player, 0, 1),
+	}
+
+	g.Rooms = map[string]*Room{
+		"кухня": {
+			Game: &g,
+			Name: "кухня",
+			Items: map[string]Item{
+				"чай": {
+					Name:  "чай",
+					Exist: false,
+				},
+			},
+		},
+		"коридор": {
+			Game:  &g,
+			Name:  "коридор",
+			Items: map[string]Item{},
+		},
+		"комната": {
+			Game: &g,
+			Name: "комната",
+			Items: map[string]Item{
+				"ключи": {
+					Name:  "ключи",
+					Exist: false,
+				},
+				"рюкзак": {
+					Name:  "рюкзак",
+					Exist: false,
+				},
+				"конспекты": {
+					Name:  "конспекты",
+					Exist: false,
+				},
+			},
+		},
+		"улица": {
+			Game:  &g,
+			Name:  "улица",
+			Items: map[string]Item{},
+		},
+	}
+
+	g.Players = []Player{
+		{
+			InRoom: g.GetRoom("кухня"),
+		},
+	}
 }
 
 func handleCommand(command string) string {
@@ -25,5 +149,23 @@ func handleCommand(command string) string {
 		данная функция принимает команду от "пользователя"
 		и наверняка вызывает какой-то другой метод или функцию у "мира" - списка комнат
 	*/
-	return "not implemented"
+	p := &g.Players[0]
+	action := strings.Split(command, " ")
+	switch action[0] {
+	case "осмотреться":
+		return p.View()
+	case "идти":
+		{
+			p.Go(g.GetRoom(action[1]).Name)
+			return "Идти"
+		}
+	case "надеть":
+		return "Надеть"
+	case "взять":
+		return "Взять"
+	case "применить":
+		return "Применить"
+	default:
+		return "неизвестная команда"
+	}
 }
